b6ef143514a64df8d1c528946295cc4409f40e9e
cescoferraro <francescoaferraro@gmail.com>
2016-10-24 17:22:40 -0200

    newbie

---
+++ cmd/version.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"bitbucket.org/cescoferraro/api/util"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Return the current version of the API",
+	Long: `Return the current version of the API.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(util.VERSION)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+
+}
